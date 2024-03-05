85344e7679dad5409a94ed9f54da3b461743fed2
Vanitha Kannan <vkgenps@gmail.com>
2024-03-05 18:00:48 +0800

    updated files

--- src/main/java/com/vk/jenkinp/JenkincicdApplication.java
+++ src/main/java/com/vk/jenkinp/JenkincicdApplication.java
@@ -4,13 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class JenkincicdApplication {
 
 	@GetMapping("/greetings/{name}")
-	public String grettings(@PathVariable String name){
-		return "Hello" + name + " Greetings from Jenkins CI-CD Flow....";
+	public String greetings(@PathVariable String name){
+		return "Hello " + name + " Greetings from Jenkins CI-CD Flow....";
 	}
 
 	public static void main(String[] args) {
--- src/test/java/com/vk/jenkinp/JenkincicdApplicationTests.java
+++ src/test/java/com/vk/jenkinp/JenkincicdApplicationTests.java
@@ -2,29 +2,26 @@ package com.vk.jenkinp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-@SpringBootTest
+@WebMvcTest
 class JenkincicdApplicationTests {
 
 	@Autowired
 	private MockMvc mockMvc;
 
 	@Test
+
 	public void testGreetingsEndpoint()throws Exception{
-		String name ="Jothi";
+		String name ="jothi";
 
 		mockMvc.perform(MockMvcRequestBuilders.get("/greetings/{name}",name))
 				.andExpect(MockMvcResultMatchers.status().isOk())
-				.andExpect(MockMvcResultMatchers.content().string("Hello" + name + " Greetings from Jenkins CI-CD Flow...."));
-
-	}
-
-	@Test
-	void contextLoads() {
+				.andExpect(MockMvcResultMatchers.content().string("Hello " + name.split(" ")[0] + " Greetings from Jenkins CI-CD Flow...."));
 	}
 
 }
